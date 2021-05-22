846ab4678cb24d3aafd468951123146850fae314
WilliamPinheiro18 <williampinheiro18@gmail.com>
2021-05-21 21:08:38 -0300

    Tentativa de acoplar o grafo no fragmento

--- Projeto/app/src/main/java/com/redesocial/control/GraphFragment.java
+++ Projeto/app/src/main/java/com/redesocial/control/GraphFragment.java
@@ -1,47 +1,85 @@
 package com.redesocial.control;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.redesocial.R;
 import com.redesocial.activity.DrawGraph;
+import com.redesocial.database.Graph;
+import com.redesocial.database.User;
 
 public class GraphFragment extends Fragment {
 
+    DrawGraph drawGraph;
+    Graph graph = new Graph();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DrawGraph drawGraph = new DrawGraph();
-        try {
-            drawGraph.Create();
 
-        }catch (Exception e){
-            System.out.println(e);
-        }
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        //getApplicationContext().getDisplay().getRealMetrics( displayMetrics );
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics( displayMetrics );
+        drawGraph.initialConfig( displayMetrics );
 
-        //drawGraph.onTouchEvent();
+        User user;
 
+        user = new User();
+        user.setName("Joao Pedro");
+        user.setEmail("0");
+        graph.addVertex( user );
+
+        user = new User();
+        user.setName("Tiago Augusto");
+        user.setEmail("1");
+        graph.addVertex( user );
+
+        user = new User();
+        user.setName("Maria Dolores");
+        user.setEmail("2");
+        graph.addVertex( user );
+
+        user = new User();
+        user.setName("Guilherme Freitas");
+        user.setEmail("3");
+        graph.addVertex( user );
+
+        user = new User();
+        user.setName("Ana Clara");
+        user.setEmail("4");
+        graph.addVertex( user );
+
+        user = new User();
+        user.setName("Marcos Vinícius");
+        user.setEmail("5");
+        graph.addVertex( user );
+
+        graph.addEdge( "0", "1" );
+        graph.addEdge( "0", "3" );
+        graph.addEdge( "1", "3" );
+        graph.addEdge( "1", "2" );
+        graph.addEdge( "2", "3" );
+        graph.addEdge( "2", "2" );
+        graph.addEdge( "3", "0" );
+        graph.addEdge( "5", "4" );
+
+
+        drawGraph.setGraph( graph );
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_graph, container, false);
+
+        drawGraph = (DrawGraph) getView().findViewById( R.id.drawGraph );
 
 
 
-        View view = inflater.inflate(R.layout.fragment_graph, container, false);
-
-       // Intent myIntent = new Intent(getActivity(), DrawGraph.class);
-        //startActivity( myIntent );
-
-        return view;
+        return inflater.inflate(R.layout.fragment_graph, container, false);
 
     }
 
