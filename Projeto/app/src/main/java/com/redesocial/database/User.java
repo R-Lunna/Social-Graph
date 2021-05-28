f3d1e35208c841479bf97963a62472aec75ab272
WilliamPinheiro18 <williampinheiro18@gmail.com>
2021-05-28 20:29:52 -0300

    Update User.java

--- Projeto/app/src/main/java/com/redesocial/database/User.java
+++ Projeto/app/src/main/java/com/redesocial/database/User.java
@@ -3,13 +3,51 @@ package com.redesocial.database;
 
 public class User {
 
+    private int id;
     private String name;
     private String email;
     private String password;
     private String urlPhoto;
     private String birthday;
     private String sex;
+    private int positionX;
+    private int positionY;
 
+    public User()
+    {
+
+    }
+
+
+    public User( int id, String name, String email, String password, String urlPhoto, String birthday, String sex, int positionX, int positionY )
+    throws IllegalArgumentException
+    {
+        if( id < 0 )
+            throw new IllegalArgumentException("Id cannot be negative");
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.urlPhoto = urlPhoto;
+        this.birthday = birthday;
+        this.sex = sex;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId( int id )
+    throws IllegalArgumentException
+    {
+        if( id < 0 )
+            throw new IllegalArgumentException("Id cannot be negative");
+
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -57,5 +95,21 @@ public class User {
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
     }
 }
