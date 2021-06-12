cd47657a426435403fb3ca162c79f9dbfd22f97d
LordFarron <brandon.brunno@gmail.com>
2021-06-12 01:21:49 -0300

    Add LocalUser

---
+++ Projeto/app/src/main/java/com/redesocial/database/LocalUser.java
@@ -0,0 +1,21 @@
+package com.redesocial.database;
+
+public class LocalUser {
+
+    private static int id;
+
+
+    public LocalUser(){
+
+    }
+
+    public LocalUser(int id){
+        this.id = id;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+
+}
